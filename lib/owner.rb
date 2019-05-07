638005f143b875e29b8180dd11885ad2287ad2d1
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-07 03:14:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,7 +53,7 @@ class Owner
   
   def sell_pets
     # binding.pry
-    @pets.each{|a| a.collect{|b| b.mood = "nervous"}}
+    @pets.each{|a| avaluescollect{|b| b.mood = "nervous"}}
     @pets.clear
   end
   
