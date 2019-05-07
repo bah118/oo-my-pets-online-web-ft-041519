553735254239f00e85139be5b5d90061e27ce08b
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-07 03:49:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,7 +59,7 @@ class Owner
   def list_pets
     "I have #{@pets[:fishes].length} fish, " +
     "#{@pets[:dogs].length} dog(s), and " +
-    "#{pets[:cats].length} cat(s)."
+    "#{@pets[:cats].length} cat(s)."
   end
   
 end
