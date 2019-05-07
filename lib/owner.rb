7946b709d8f97565d2e314584f917a19fff7a9c0
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-07 03:49:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,7 +52,7 @@ class Owner
   end
   
   def sell_pets
-    @pets.values.flatten.each {|pet| pet.mood = "nervous"}
+    @pets.values.flatten.each{|pet| pet.mood = "nervous"}
     @pets.clear
   end
   
